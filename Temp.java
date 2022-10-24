0988bed39b7901b2273d43b176fcc5700ac121e5
Joezishere <Joseph.zinni@vtsd.con>
2022-10-24 07:36:50 -0400

    Mondays homework

---
+++ Temp.java
@@ -0,0 +1,14 @@
+public class Temp {
+    public static void main(String args[]) {
+
+
+        for (int v = 100; v >= 0; v -= 3) {
+            System.out.print(v + ",");
+
+
+        }
+
+
+    }
+
+}
